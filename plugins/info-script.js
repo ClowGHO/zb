7004c814d8b3d1a8df51969339f05eb9ecb4c0b9
ClowGHO <darmabakti000@gmail.com>
2022-12-04 18:54:43 +0700

    zb

---
+++ plugins/info-script.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://ghofams-official.up.railway.app/*
+Official Ghofamd™`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'O W N E R','.owner', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/khoirul36_",
+    mediaType: "VIDEO",
+    description: "https://ghofams-official.up.railway.app", 
+    title: 'Ghofams™',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: urlnya
+  }
+  } }) 
+          }
+handler.help = ['script']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+module.exports = handler
